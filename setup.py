3ac94c1b8b9fdd23cc7fffac478b0b1119e73e68
Veikkan <pierre.semanne@gmail.com>
2023-10-26 15:16:53 +0200

    lib setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='matriciefy',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='0xMugen',
+    description='A DL project from scratch',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/0xMugen/DL-for-scratch',
+)
---
+++ testing/test.py
@@ -0,0 +1,4 @@
+from dlproc import matriciefy as mat
+from dlproc import neuralnet as neu
+print(mat.add(5,3))
+print(neu.subtract(5,3))
